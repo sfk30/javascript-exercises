cf5fcc1d6a5dc46e2273136c790d3d504fc64663
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-24 20:44:02 +0100

    Completed ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringToArray = Array.from(string);
+    revStringToArray = stringToArray.reverse()
+    revStringToArrayJoined = revStringToArray.join('')
+    return revStringToArrayJoined
 };
 
 // Do not edit below this line
