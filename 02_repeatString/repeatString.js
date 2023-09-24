208d817391fbd6d432fb770627da95db53633203
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-24 20:30:40 +0100

    Finished ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+    empty = ''
+    for (i = 0; i < num; i++) {
+        empty += string
+    }
+    return empty
+    }
 };
 
 // Do not edit below this line
