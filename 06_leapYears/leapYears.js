b1520834dad034745a9f8b61b6ab7db44482bc91
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-25 14:12:47 +0100

    Finished ex 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
-
+function leapYears(year) {
+    if (year % 4 == 0 && year % 100 !== 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 400 == 0) { 
+        return true;
+    } else {
+        return false;
+    }
+}
 // Do not edit below this line
 module.exports = leapYears;
+
