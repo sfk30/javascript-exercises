35f5d61757d632d802cf3cfb4e37ccbf2ca34b93
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-25 14:46:28 +0100

    Completed ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,10 @@
-const convertToCelsius = function() {
-};
 
-const convertToFahrenheit = function() {
-};
+function convertToCelsius(fahrenheit) { 
+  return Math.round(((fahrenheit - 32) * (5/9)) * 10)/10
+}
+function convertToFahrenheit(celsius) { 
+  return Math.round((celsius * (9/5) + 32) * 10)/10;
+}
 
 // Do not edit below this line
 module.exports = {
