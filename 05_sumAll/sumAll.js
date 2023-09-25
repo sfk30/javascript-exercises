620fdb6e72b19e7c405e5064f71d8b2215709ae9
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-25 13:34:11 +0100

    Finished ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
 
-};
+function sumAll(a,b) {
+    total = 0;
+    if (typeof(a) != 'number' || typeof(b) != 'number') {
+        return 'ERROR'
+    }
+    else if (a < 0 || b < 0){
+        return 'ERROR'
+    }
+    else if (Math.min(a,b) === a) {
+        min = a
+        max = b
+    }
+    else { 
+        min = b
+        max = a
+    }
+    for (i = min; i <= max; i++) {
+        total += i
+    }
+    return total
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// function sumAll(a,b) {
+//     total = 0;
+//     for (i = a; i <= b; i++) {
+//         total += i
+//     }
+//     return total
+// }
+
