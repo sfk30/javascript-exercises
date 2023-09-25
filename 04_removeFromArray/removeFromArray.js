d3205c5553bb081b2e8fb573dbd3522bfbe083ff
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-25 11:52:52 +0100

    Finished ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
 
-};
+
+function removeFromArray(arr, ...args) {
+    return arr.filter(function(arrItem) {
+        return !args.includes(arrItem)
+    })
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// function removeFromArray(arr, n) {
+//     const index = arr.indexOf(n)
+//     if (index > -1) {
+//         arr.splice(index,1);
+//     }
+//     return arr
+// }
+
+
+// function removeFromArray(arr, ...args) {
+//     return arr.filter(function(arrItem) {
+//         return !args.includes(arrItem)
+//     })
+// }
+
+// const removeFromArray = (arr, ...args) => arr.filter((item) => !args.includes(item))
