caa25e5e82564e04b025024c76955dc201233691
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-30 14:35:52 +0100

    Completed ex_10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = [1,1]
+    if (num === 1 || num === 2) {
+        return 1;
+    } 
+    if (num <= 0) {
+        return 'OOPS';
+    } 
+    for (let i = 3; i <= num; i ++) {
+        let numberToAdd = arr[arr.length - 1] + arr[arr.length - 2]
+        // console.log(numberToAdd)
+        arr.push(numberToAdd)
+        // console.log(arr)
+    }
+    return arr[arr.length - 1] 
 };
 
 // Do not edit below this line
