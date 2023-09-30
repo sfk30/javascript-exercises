9e6845b2925a4363640c855136837c93c3e7e244
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-30 12:33:07 +0100

    Done ex _08 and ex_09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+// const add = function(a, b) {  
+//   return a + b
+// };
+
+const add = (a,b) => a + b 
+
+// const subtract = function() {
 	
-};
+// };
 
-const subtract = function() {
+const subtract = (a,b) => a - b
+
+const sum = function(array) {return array.reduce((sum, current) => sum += current, 0)};
+
+// const sum = array.reduce((sum, current) => sum += current, 0)
 	
-};
+// }; doesn't work as array not defined 
 
-const sum = function() {
-	
-};
+const multiply = function(array) {return array.reduce((total, num) => total * num)};
 
-const multiply = function() {
+// when there's no initial value the reduce method takes the first element and sets it as initial 
+// so [1,2,3] total = 1 in this case we could also do:
+// const multiply = function(array) {return array.reduce((num1, num2) => num1 * num2)};
+// num1 just becomes the total after each round 
+const power = (a,b) => a**b
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) { 
+  if (a === 0 || a === 1) {
+    return 1
+  } for (let i = a - 1; i >= 1; i--) {
+    a *= i
+  }
+  return a
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+// const palindromes = function(string) {
+//     const reverseString = Array.from(string).reverse().join('').replace(/[^a-z0-9]/g, '');
+//     if (reverseString === string) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }; doesnt work as for e.g. string ='racecar!' but reverseString = 'racecar' 
 
+
+const palindromes = function(string) {
+    const checkString = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+    // console.log(checkString)
+    const reverseString = Array.from(checkString).reverse().join('')
+    // console.log(Array.from(checkString))
+    // console.log(Array.from(checkString).reverse())
+    // console.log(reverseString)
+    if ( reverseString === checkString) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
